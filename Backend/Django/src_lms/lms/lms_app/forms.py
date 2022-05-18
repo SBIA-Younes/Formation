4420f206bae78af263ed4038b7a3ab7c7cad6db2
Younes3113 <brahim-sb@hotmail.fr>
2022-05-18 23:09:24 +0200

    Modificaten to form in back

--- Backend/Django/src_lms/lms/lms_app/forms.py
+++ Backend/Django/src_lms/lms/lms_app/forms.py
@@ -9,5 +9,31 @@ class BookForm(forms.ModelForm):
         """Meta definition for Bookform."""
 
         model = Book
-        fields = "__all__"
+        fields = [
+            "title", 
+            "author",
+            "photo_book",
+            "photo_author",
+            "pages",
+            "price",
+            "retal_price_day",
+            "retal_period",
+            "status",
+            "category",
+            
+            ]
+        
+        widgets = {
+            "title": forms.TextInput(attrs={"class":"form-control"}),
+            "author": forms.TextInput(attrs={"class":"form-control"}),
+            "photo_book": forms.FileInput(attrs={"class":"form-control"}),
+            "photo_author": forms.FileInput(attrs={"class":"form-control"}),
+            "pages": forms.NumberInput(attrs={"class":"form-control"}),
+            "price": forms.NumberInput(attrs={"class":"form-control"}),
+            "retal_price_day": forms.NumberInput(attrs={"class":"form-control"}),
+            "retal_period": forms.NumberInput(attrs={"class":"form-control"}),
+            "status": forms.Select(attrs={"class":"form-control"}),
+            "category": forms.Select(attrs={"class":"form-control"}),
+            
+            }
         
--- Backend/Django/src_lms/lms/lms_app/views.py
+++ Backend/Django/src_lms/lms/lms_app/views.py
@@ -1,14 +1,21 @@
 from multiprocessing import context
 from django.shortcuts import render
+from .forms import BookForm
 
 from .models import *
 # Create your views here.
 
 
 def index(request):
+    
+    if request.method == 'POST':
+        add_book = BookForm(request.POST, request.FILES)
+        if add_book.is_valid():
+            add_book.save()
     context = {
         "category" : Category.objects.all(),
         "books": Book.objects.all(),
+        "form" : BookForm,
     }
     return render(request, 'pages/index.html',context=context)
 
