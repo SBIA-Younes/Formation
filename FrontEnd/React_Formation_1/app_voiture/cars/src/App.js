bd6ed0cb8afa9da1e6e1f2182e08e36208962c1c
Younes3113 <brahim-sb@hotmail.fr>
2022-07-19 13:29:40 +0200

    terminer cours 1 react

--- FrontEnd/React_Formation_1/app_voiture/cars/src/App.js
+++ FrontEnd/React_Formation_1/app_voiture/cars/src/App.js
@@ -1,16 +1,51 @@
 import React, { Component } from "react";
-import Mycars from './components/Mycars'
+import Mycars from "./components/Mycars";
 import "./App.css";
 
 class App extends Component {
   state = {
-    title: 'Mon Catalogue Voiture',
+    title: "Mon Catalogue Voiture",
+  };
+
+  changeTitle = (e) => {
+    this.setState({ title: "Mon Nouveau Titre" });
+  };
+
+  changeViaParrm = (title) => {
+    this.setState({ title: title });
+  };
+
+  changeViaBind = (title) => {
+    this.setState({ title: title });
+  };
+
+  changeViaInput = (e) => {
+    this.setState({ title: e.target.value });
   };
 
   render() {
     return (
       <div className="App">
-        <Mycars title={this.state.title}/>
+        <Mycars title={this.state.title} />
+        <button onClick={this.changeTitle}>Change le titre</button>
+
+        <button
+          onClick={() => this.changeViaParrm("Change le Titre Via Parameters")}
+        >
+          Change Via Parameters
+        </button>
+
+        <button
+          onClick={this.changeViaBind.bind(this, "change Titre Via Bind")}
+        >
+          Change Via Bind
+        </button>
+
+        <input
+          type="text"
+          onChange={this.changeViaInput}
+          value={this.state.title}
+        />
       </div>
     );
   }
--- FrontEnd/React_Formation_1/app_voiture/cars/src/components/Mycars.js
+++ FrontEnd/React_Formation_1/app_voiture/cars/src/components/Mycars.js
@@ -2,12 +2,25 @@ import React, { Component } from "react";
 import Car from "./Cars";
 
 class Mycars extends Component {
+  noCopy = (event) => {
+    alert("Merci de ne  pas copier le texte");
+    event.preventDefault();
+  };
+
+  addStyled = (e) => {
+    console.log(e);
+    e.target.classList.toggle("styled");
+  };
 
   render() {
     // console.log();
     return (
       <div>
-        <h1>{this.props.title}</h1>
+        <h1 onMouseOver={this.addStyled}>{this.props.title}</h1>
+
+        <p onCopy={this.noCopy}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        </p>
 
         <Car color="red">Ford</Car>
         <Car>Mercedes</Car>
