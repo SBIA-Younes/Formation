195f494fda59615493b49aa7a37461e0c66b188d
Younes3113 <brahim-sb@hotmail.fr>
2022-07-21 08:45:40 +0200

    resolve le TP navScroll

--- FrontEnd/fontFormation_2/les TP/TP_7_nav_scroll/index.js
+++ FrontEnd/fontFormation_2/les TP/TP_7_nav_scroll/index.js
@@ -5,3 +5,15 @@
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
 // Connaitre niveau de scroll (window.scrollY)
+lasteScroll = 0;
+nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY < lasteScroll) {
+    nav.style.top = "0px";
+  } else {
+    nav.style.top = "-60px";
+  }
+
+  lasteScroll = window.scrollY;
+});
