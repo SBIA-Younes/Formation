2b2c428dbd3557b50dbb0b6e85b16f3131a6b707
Younes3113 <brahim-sb@hotmail.fr>
2022-07-24 20:07:32 +0200

    corection scrol_navbar

--- FrontEnd/fontFormation_2/les TP/TP_8_scroll_popup/script.js
+++ FrontEnd/fontFormation_2/les TP/TP_8_scroll_popup/script.js
@@ -2,29 +2,30 @@
 console.log(imgImprovise);
 window.addEventListener("scroll", () => {
   // Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
-  if (window.scrollY) {
+  if (window.scrollY > 50) {
     navbar.style.height = "50px";
-  } else if (window.scrollY == 0) {
+  } else {
     navbar.style.height = "90px";
   }
   // Faire apparaitre l'image de la partie improvise
-  if (window.scrollY >= 200) {
+  /*   console.log(document.body.offsetHeight); // pour recuperer la longuer de la page */
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight; // pour recuperer le pourcentage
+
+  if (scrollValue > 0.45) {
     imgImprovise.style.opacity = "1";
-    imgImprovise.style.transform = "translateX(0)";
-  }
-  // Faire apparaitre la popup quand on est en bas du site
-  if (window.scrollY >= 1217) {
-    popup.style.opacity = "1";
-    popup.style.transform = "translateX(0)";
+    imgImprovise.style.transform = "none";
   }
 
+  // Faire apparaitre la popup quand on est en bas du site
+  if (scrollValue > 0.85) {
+    popup.style.opacity = "1";
+    popup.style.transform = "none";
+  }
   // Bonus : quand on clicke sur la popup elle disparait pour toujours
-  closeBtn.addEventListener(
-    "click",
-    () => {
-      popup.style.opacity = "0";
-      popup.style.visibility = "hidden";
-    },
-    false
-  );
+  closeBtn.addEventListener("click", () => {
+    popup.style.opacity = "0";
+    popup.style.transform = "translateX(500px)";
+    popup.style.visibility = "hidden";
+  });
 });
