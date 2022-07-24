e666121f0013b3a215dfdd0b4c6d4bb88f2d7ec1
Younes3113 <brahim-sb@hotmail.fr>
2022-07-24 14:42:11 +0200

    resolv le TP scroll_popup

--- FrontEnd/fontFormation_2/les TP/TP_8_scroll_popup/script.js
+++ FrontEnd/fontFormation_2/les TP/TP_8_scroll_popup/script.js
@@ -1,23 +1,30 @@
 // Créer un événement au scroll
-
 console.log(imgImprovise);
 window.addEventListener("scroll", () => {
+  // Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
   if (window.scrollY) {
     navbar.style.height = "50px";
   } else if (window.scrollY == 0) {
     navbar.style.height = "90px";
-  } else if (window.scrollY == 200) {
-    console.log(window.scrollY);
-    console.log("scrollY >= 200");
+  }
+  // Faire apparaitre l'image de la partie improvise
+  if (window.scrollY >= 200) {
     imgImprovise.style.opacity = "1";
     imgImprovise.style.transform = "translateX(0)";
   }
+  // Faire apparaitre la popup quand on est en bas du site
+  if (window.scrollY >= 1217) {
+    popup.style.opacity = "1";
+    popup.style.transform = "translateX(0)";
+  }
+
+  // Bonus : quand on clicke sur la popup elle disparait pour toujours
+  closeBtn.addEventListener(
+    "click",
+    () => {
+      popup.style.opacity = "0";
+      popup.style.visibility = "hidden";
+    },
+    false
+  );
 });
-
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
-
-// Faire apparaitre l'image de la partie improvise
-
-// Faire apparaitre la popup quand on est en bas du site
-
-// Bonus : quand on clicke sur la popup elle disparait pour toujours
