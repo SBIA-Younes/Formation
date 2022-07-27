e90547c0117f58c750de738171acfe2de44bec62
Younes3113 <brahim-sb@hotmail.fr>
2022-07-27 02:24:11 +0200

    reolv TP_Sidebar a 80%

--- FrontEnd/fontFormation_2/les TP/TP_9_Sidebar/index.js
+++ FrontEnd/fontFormation_2/les TP/TP_9_Sidebar/index.js
@@ -1,5 +1,23 @@
 // Créer un événement au click sur le hamburger menu pour sortir la sidebar, ranger la sidebar si on reclick dessus
 
+const navbarToggle = document.querySelectorAll("[data-nav-toggler]");
+
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarToggle.length; i++) {
+  navbarToggle[i].addEventListener("click", () => {
+    side_bar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", () => {
+    side_bar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
 // Ranger la sidebar si on click sur le contenu principal
 
 // BONUS
