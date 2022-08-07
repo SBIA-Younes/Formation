20710ac28af6346dea2627aae69e18bbdf04186d
Younes3113 <brahim-sb@hotmail.fr>
2022-08-07 18:55:53 +0200

    modif form

---
+++ FrontEnd/fontFormation_2/JavaScript/tp/form-checker/index.js
@@ -0,0 +1,65 @@
+const inputs = document.querySelectorAll(
+  'input[type="text"], input[type="password"]'
+);
+
+let pseudo, email, password, confirmPass;
+
+const errorDisplay = (tag, message, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
+
+  if (!valid) {
+    container.classList.add("error");
+    span.textContent = message;
+  } else {
+    container.classList.remove("error");
+    span.textContent = message;
+  }
+};
+
+const pseudoChecker = (value) => {
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay("pseudo", "Le pseudo doit faire entre 3 et 20 cartères");
+    pseudo = null;
+  } else if (!value.match(/^[a-z-0-9_.-]*$/)) {
+    errorDisplay("pseudo", "Le ne doit pas contenire de caractères spéciaux");
+    pseudo = null;
+  } else {
+    errorDisplay("pseudo", "", true);
+    pseudo = value;
+    console.log(pseudo);
+  }
+};
+const emailChecker = (value) => {
+  console.log(value);
+};
+
+const passwordChecker = (value) => {
+  console.log(value);
+};
+
+const confirmChecker = (value) => {
+  console.log(value);
+};
+
+inputs.forEach(function (input) {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "pseudo":
+        pseudoChecker(e.target.value);
+        break;
+
+      case "email":
+        emailChecker(e.target.value);
+        break;
+
+      case "password":
+        passwordChecker(e.target.value);
+        break;
+
+      case "confirm":
+        confirmChecker(e.target.value);
+        break;
+    }
+  });
+});
