9cf3cf325b4a9203359c99dcd41b8d5f0835f7a6
Younes3113 <brahim-sb@hotmail.fr>
2022-08-08 20:36:32 +0200

    creation de page single post et blog et model

--- Backend/Django/formation_watsapp/src_blog/post/models.py
+++ Backend/Django/formation_watsapp/src_blog/post/models.py
@@ -1,3 +1,21 @@
+from audioop import reverse
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=10000)
+    image = models.ImageField(upload_to="post/")
+
+    class Meta:
+        verbose_name = ("post")
+        verbose_name_plural = ("posts")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
--- Backend/Django/formation_watsapp/src_blog/post/views.py
+++ Backend/Django/formation_watsapp/src_blog/post/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Post
+
+
+def all_posts(request):
+    post = Post.objects.all()
+    context = {
+        "posts": post
+    }
+    return render(request, "posts.html", context)
+
+
+def single_post(request, id):
+    post = Post.objects.get(id=id)
+    context = {
+      "single_post": post
+    }
+    return render(request, "single.html", context)
