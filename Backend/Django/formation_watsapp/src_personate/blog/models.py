6510700d25e2ce4c5814f2d0f2e06ed6c279f312
Younes3113 <brahim-sb@hotmail.fr>
2022-08-17 18:32:20 +0200

    designe models blog

--- Backend/Django/formation_watsapp/src_personate/blog/models.py
+++ Backend/Django/formation_watsapp/src_personate/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts/")
+    publish_date = models.DateTimeField(default=timezone.now())
+    content = models.TextField(max_length=10000)
+
+    def __str__(self):
+        return self.title
+
