172f3af763352d9140ca8e990aabd96584d1ef97
Younes3113 <brahim-sb@hotmail.fr>
2022-08-18 18:33:27 +0200

    structure de done du webesite

--- Backend/Django/formation_watsapp/src_personate/blog/views.py
+++ Backend/Django/formation_watsapp/src_personate/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def all_posts(request):
+  objects = Post.objects.all()
+  context = {"posts" : objects}
+  return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, id):
+  pass
